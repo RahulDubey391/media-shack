e6c9f8aa74a477fef19d903e30f8f2b9517d35f8
Rahul Dubey <100185371+RahulDubey391@users.noreply.github.com>
2024-03-28 01:04:08 +0530

    Update core.views.py

--- src/core/views.py
+++ src/core/views.py
@@ -1,26 +1,53 @@
 from flask import Blueprint, render_template, request, jsonify, render_template_string
+from src.models import MediaPost
+from flask import current_app
+from src import app
 import math
 import os
 
 core = Blueprint('core', __name__)
 
+# def loadGallery():
+#     images_dir = os.path.join(os.getcwd(), 'src', 'static', 'images')
+#     print(images_dir)
+#     media_list = []
+
+#     for i, filename in enumerate(os.listdir(images_dir)):
+#         if filename.endswith(('.jpg', '.jpeg', '.png', '.gif')):
+#             image_info = {
+#                 "id": i,
+#                 "title": filename.split('.')[0].replace('pexels-',''),
+#                 "thumbnail": filename,
+#                 "description": f"Description for {filename.split('.')[0]}",
+#                 "url": 'sample_video.mp4'
+#             }
+
+#             media_list.append(image_info)
+
+#     return media_list
+
 def loadGallery():
-    images_dir = os.path.join(os.getcwd(), 'src', 'static', 'images')
-    print(images_dir)
     media_list = []
 
-    for i, filename in enumerate(os.listdir(images_dir)):
-        if filename.endswith(('.jpg', '.jpeg', '.png', '.gif')):
-            image_info = {
-                "id": i,
-                "title": filename.split('.')[0].replace('pexels-',''),
-                "thumbnail": filename,
-                "description": f"Description for {filename.split('.')[0]}",
-                "url": 'sample_video.mp4'
-            }
+    with app.app_context():
 
+        # Query MediaPost objects from the database
+        media_posts = MediaPost.query.all()
+
+        print('Media Posts : ', media_posts)
+
+        for post in media_posts:
+            # Assuming thumbnail_url is the field in MediaPost representing the image thumbnail
+            image_info = {
+                "id": post.content_id,  # Assuming MediaPost has an 'id' field
+                "title": post.content_title,
+                "thumbnail": post.thumbnail_url.split('/')[-1],
+                "description": f"Description for {post.content_title}",
+                "url": post.video_url
+            }
             media_list.append(image_info)
 
+    print('Media List length : ', len(media_list))
     return media_list
 
 videos = loadGallery()
